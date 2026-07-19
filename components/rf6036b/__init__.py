43aa6b558805771b7ac1121d32039e7168c4f866
elektroleak <dennis.evers999@gmail.com>
2026-07-19 09:09:12 +0200

    Fehlerkorrektur

--- components/rf6036b/__init__.py
+++ components/rf6036b/__init__.py
@@ -3,7 +3,7 @@ import esphome.config_validation as cv
 from esphome.components import remote_base
 from esphome.const import CONF_ID
 
-CODEOWNERS = ["@elektroleak"]
+CODEOWNERS = ["@dein-github-user"]  # <-- anpassen
 DEPENDENCIES = ["remote_receiver"]
 
 rf6036b_ns = cg.esphome_ns.namespace("rf6036b")
